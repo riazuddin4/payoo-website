9122bf7a31ac91113fb14d5f6905e5155490fe9e
riazuddin4 <mdriaj2010bd@gmail.com>
2026-02-19 12:00:55 +0600

    java

--- script/login.js
+++ script/login.js
@@ -1,1 +1,23 @@
-console.log('hello')
+console.log('Login functionality commign');
+
+document.getElementById("login-btn").addEventListener("click", function(){
+    //1 get the mobile number input
+    const numberInput = document.getElementById("input-number");
+    const contactNumber = numberInput.value;
+    console.log(contactNumber);
+    //2 get the pin input
+    const inputPin = document.getElementById("input-pin");
+    const pin =inputPin.value;
+    console.log(pin);
+    //3 match pin & mobile number
+    if(contactNumber == "01221258040" && pin == "2010"){
+        alert("login Success");
+        // window.location.replace("/home.html");
+        window.location.assign("/home.html");
+    }else{
+        alert("login Failes");
+        return;
+    }
+
+
+});
